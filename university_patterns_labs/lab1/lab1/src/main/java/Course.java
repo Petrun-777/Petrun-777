46362675d71832e93b1f9431ecbdb2a17285a0e7
Petrun' Dmytro <74406631+Petrun-777@users.noreply.github.com>
2021-11-29 18:57:47 +0200

    Add files via upload

---
+++ university_patterns_labs/lab1/lab1/src/main/java/Course.java
@@ -0,0 +1,65 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+/**
+ * by PetrunDmytro
+ */
+@Getter
+@Setter
+@Slf4j
+public class Course {
+
+   private String name;
+   private Date date;
+   private int seminarsNumber;
+   private float fee;
+   private Professor professor;
+   private Map<Student,Enrollment> enrolledStudents = new HashMap<>();
+
+   public Course(String name, Date date,Professor professor, int seminarsNumber, float fee) {
+      this.name = name;
+      this.date = date;
+      this.seminarsNumber = seminarsNumber;
+      this.fee = fee;
+      log.info("Course was created: "+this);
+   }
+   public Course(String name, Date date, int seminarsNumber, float fee) {
+       this(name,date,null,seminarsNumber,fee);
+   }
+
+   public void addStudent(Student student,Enrollment enrollment){
+      enrolledStudents.put(student,enrollment);
+      log.info("Successfully added new Student : "+student.getName());
+   }
+
+   public void removeStudent(Student student ){
+       enrolledStudents.remove(student);
+      log.info("Successfully removed Student : "+student.getName());
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Course course = (Course) o;
+      return seminarsNumber == course.seminarsNumber && Float.compare(course.fee, fee) == 0 && Objects.equals(name, course.name) && Objects.equals(date, course.date);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(name, date, seminarsNumber, fee);
+   }
+
+   @Override
+   public String toString() {
+      return "Course{" +
+              "name='" + name + '\'' +
+              ", date=" + date +
+              ", seminarsNumber=" + seminarsNumber +
+              ", fee=" + fee +
+              '}';
+   }
+}
---
+++ university_patterns_labs/lab1/lab1/src/main/java/Enrollment.java
@@ -0,0 +1,57 @@
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * by PetrunDmytro
+ */
+@Data
+@Slf4j
+public class Enrollment {
+    private List<Mark> receivedMarks = new ArrayList<>();
+
+    public Enrollment() {
+        log.info("Enrollment was created");
+    }
+
+    public float getAverageToDate(LocalDate date) {
+        List<Mark> marksToDate = receivedMarks.stream()
+                .filter(E->E.date.isBefore(date)||E.date.isEqual(date))
+                .collect(Collectors.toList());
+        return getAverage(marksToDate);
+    }
+
+    public float getFinalMark() {
+        return getAverage(receivedMarks);
+    }
+
+    public void addMark(Integer mark) {
+        addMark(LocalDate.now(), mark);
+    }
+
+    public void addMark(LocalDate date, Integer mark) {
+        receivedMarks.add(new Mark(date, mark));
+        log.info("Added new Mark: " + mark + ";received at " + date);
+    }
+
+    private float getAverage(Collection<Mark> marks) {
+        int sum = 0;
+        for (Mark mark : marks) {
+            sum += mark.value;
+        }
+        return sum / (float) marks.size();
+    }
+
+    private class Mark {
+        public Mark(LocalDate date, Integer value) {
+            this.date = date;
+            this.value = value;
+        }
+
+        LocalDate date;
+        Integer value;
+    }
+}
