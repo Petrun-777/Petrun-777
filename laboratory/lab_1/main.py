21ec5524d82e0d3edfafa49c63a2071efb221921
Petrun' Dmytro <74406631+Petrun-777@users.noreply.github.com>
2021-11-29 19:04:47 +0200

    Delete laboratory/lab_1 directory

--- laboratory/lab_1/main.py
+++
@@ -1,8 +0,0 @@
-from employyes import Developer
-from project import Project
-from datetime import datetime
-
-if __name__ == '__main__':
-    developer = Developer(id_=1,first_name='test_name',last_name='test_surname',address='test_address',position='junior',rank='0.7',phone='111111',email='test@test.test')
-    project = Project(title='test',start_date=datetime.now())
-    developer.assign(project=project)
